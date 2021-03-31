a13c0ac14b92d54900d29c2b978a6122fe97194a
Radeon546 <stefke.mail@gmail.com>
2021-04-01 00:01:14 +0200

    Setup main screen
    Added constants file

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF56C596);
+const kPrimaryLightColor = Color(0xFFFFFFFF);
+const kTextColor = Color(0xFF205072);
+const kSecondaryColor = Color(0xFF329D9C);
+
+const kAnimationDuration = Duration(milliseconds: 200);
